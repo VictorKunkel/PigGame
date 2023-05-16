857b6b8466b946ebb95ccfd9d1a7288aec9fdff5
VictorKunkel <victorkunkell@gmail.com>
2023-05-16 19:01:40 -0300

    Holding current score

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const switchPlayer = function () {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
 //Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -20,8 +28,10 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 btnRoll.addEventListener('click', function () {
+    if (playing) {
     const dice = Math.trunc(Math.random()* 6) + 1;
     console.log(dice);
     diceEl.classList.remove('hidden');
@@ -30,10 +40,21 @@ btnRoll.addEventListener('click', function () {
         currentScore += dice;
         document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
     } else {
-        document.querySelector(`#current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        switchPlayer();
     }
+}
 });
+
+btnHold.addEventListener('click', function () {
+    if (playing) {
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
+    if (scores[activePlayer] >= 20) {
+        playing = false;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    } else {
+    switchPlayer();
+    }
+}
+})
