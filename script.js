69c4d37630e4acc277df71a7a88b7e62317d8a53
VictorKunkel <victorkunkell@gmail.com>
2023-05-17 02:45:31 -0300

    Refactoring the code

--- script.js
+++ script.js
@@ -20,35 +20,27 @@ const switchPlayer = function () {
     player1El.classList.toggle('player--active');
 }
 
-//Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
 
 const start = function () {
-    playing = true;
-    activePlayer = 0;
+    scores = [0, 0];
     currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-    scores[0] = 0;
-    scores[1] = 0;
-
-    document.querySelector(`#current--0`).textContent = currentScore;
-    document.querySelector(`#current--1`).textContent = currentScore;
-
-    document.querySelector(`#score--0`).textContent = scores[0];
-    document.querySelector(`#score--1`).textContent = scores[1];
-
-    document.querySelector(`.player--0`).classList.remove('player--winner');
-    document.querySelector(`.player--1`).classList.remove('player--winner');
-
-    document.querySelector(`.player--0`).classList.add('player--active');
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
 };
+
+start();
 
 btnRoll.addEventListener('click', function () {
     if (playing) {
@@ -79,6 +71,4 @@ btnHold.addEventListener('click', function () {
 }
 })
 
-btnNew.addEventListener('click', function() {
-    start();
-});
+btnNew.addEventListener('click', init);
