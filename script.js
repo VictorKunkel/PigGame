e0e4a515e5a92ba76f13efe9e79cd47ccbc68a8f
VictorKunkel <victorkunkell@gmail.com>
2023-05-16 23:13:40 -0300

    Input of the restart in the game

--- script.js
+++ script.js
@@ -30,10 +30,29 @@ let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
 
+const start = function () {
+    playing = true;
+    activePlayer = 0;
+    currentScore = 0;
+
+    scores[0] = 0;
+    scores[1] = 0;
+
+    document.querySelector(`#current--0`).textContent = currentScore;
+    document.querySelector(`#current--1`).textContent = currentScore;
+
+    document.querySelector(`#score--0`).textContent = scores[0];
+    document.querySelector(`#score--1`).textContent = scores[1];
+
+    document.querySelector(`.player--0`).classList.remove('player--winner');
+    document.querySelector(`.player--1`).classList.remove('player--winner');
+
+    document.querySelector(`.player--0`).classList.add('player--active');
+};
+
 btnRoll.addEventListener('click', function () {
     if (playing) {
     const dice = Math.trunc(Math.random()* 6) + 1;
-    console.log(dice);
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
     if (dice !== 1) {
@@ -49,8 +68,9 @@ btnHold.addEventListener('click', function () {
     if (playing) {
     scores[activePlayer] += currentScore;
     document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
         playing = false;
+        diceEl.classList.add('hidden');
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
         document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
     } else {
@@ -58,3 +78,7 @@ btnHold.addEventListener('click', function () {
     }
 }
 })
+
+btnNew.addEventListener('click', function() {
+    start();
+});
